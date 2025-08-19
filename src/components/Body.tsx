f856a8c60db1aaf4c266fc0e25182b3c92a21797
Anas-Faiz <anasfaiz0811@gmail.com>
2025-08-19 07:50:23 -0700

    data layer added

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,26 +1,48 @@
-import Card from  "./Card"
+import { useEffect, useState } from "react";
+import Card from "./Card";
 
-const Body = ()=>{
+const Body = () => {
+  const [resInfo, setResInfo] = useState<any>(null);
+  const api_key = import.meta.env.VITE_API_KEY;
 
-    return(
-        <div className="body-container">
-        <div className="Search-box">
-            <input type="search" name="" id="" />
-            <button>Search</button>
-        </div>
-        <div className="card-container">
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-            <Card></Card>
-        </div>
-        
-        </div>
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    )
-}
-export default Body
+  const fetchData = async () => {
+    const res = await fetch(api_key);
+    const json = await res.json();
+    console.log(json)
+    setResInfo(json);
+  };
+
+  const path =
+    resInfo?.data?.cards?.[4]?.card?.card?.gridElements?.infoWithStyle
+      ?.restaurants || [];
+
+   // console.log (path);  
+
+  return (
+    <div className="body-container">
+      <div className="Search-box">
+        <input type="search" />
+        <button>Search</button>
+      </div>
+
+      <div className="card-container">
+        {path.map((item: any) => (
+          <Card
+            key={item.info.id}
+            image= {item.info.cloudinaryImageId}
+            name={item.info.name}
+            description={item.info.cuisines}
+            price={item.info.costForTwo}
+            rating={item.info.avgRating}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
