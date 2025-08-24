7d1273160af75e984e5a0ad30125e79f0a26e2b6
Anas-Faiz <anasfaiz0811@gmail.com>
2025-08-24 10:57:16 -0700

    about us page yap

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,10 @@
 const About = ()=>{
     return <div>
         <h1>about us</h1>
+        <h2>Anas Faiz</h2>
+        <p>an aspiring web app developer with proficiency in react , node js , sql , express js and next js <br/> think of a app and i will build it </p>
+        <h1>About This App</h1>
+        <p>I have build this web app using react js implementing industry level practices </p>
     </div>
 }
 
