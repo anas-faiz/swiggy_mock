74b5a5fcd69529a1f24c3fc3e7012720a66fb231
Anas-Faiz <anasfaiz0811@gmail.com>
2025-08-24 00:21:30 -0700

    ui improved for home page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,20 @@
-const Card = ({name,description,price,rating,image})=>{
-//console.log(resData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+const Card = ({ name, description, price, rating, image }) => {
 
-//const path = resData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants?.name
-  //  console.log(path)
-  const imageapi = import.meta.env.VITE_IMAGE_KEY
-    return(
-        <div className="card">
-                <img src={imageapi+image}/>
-                <div className="content-list">
-                    <ul>
-                        <li><strong>{name}</strong></li>
-                        <li>{price}</li>
-                        <li>{rating}👍</li>
-                        <li>{description.join(",")}</li>
-                    </ul>
-                </div>
-        </div>
-    )
-}
+    const imageapi = import.meta.env.VITE_IMAGE_KEY;
+
+    return (
+    <div className="card">
+      <img src={imageapi + image} alt={name} />
+      <div className="content-list">
+        <ul>
+          <li><strong>{name}</strong></li>
+          <li>{price}</li>
+          <li>{rating} 👍</li>
+          <li className="description">{description.join(",")}</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
 export default Card
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,18 +8,18 @@ const Header = ()=>{
   const action = ()=>{
      setLoginLogout(!loginLogout)
   }
-
+    const app_logo = import.meta.env.VITE_LOGO;
   return (
     <div className="header">
       <div className="logo">
-        <img src="https://template.canva.com/EAGMppVJ_aY/1/0/1600w-S2LGeYCK5eM.jpg" alt="logo" />
+        <img src={app_logo} alt="logo" />
         <h1>Brocelle</h1>
       </div>
       <div className="links">
-        <h1><Link to = "/"> Home </Link></h1>
-        <h1 ><Link to = "/dineout">Dineout</Link></h1>
-          <Link to= "/about"><h1>About</h1></Link>
-        <h1 className="cart"><CiShoppingCart size={50}/></h1>
+        <Link to = "/"><h1> Home </h1></Link>
+        <Link to = "/dineout"><h1>Dineout</h1></Link>
+        <Link to= "/about"><h1>About</h1></Link>
+          <Link><h1><CiShoppingCart size={50}/></h1></Link>
         <button onClick={action}>{loginLogout ? "login" : "logout"}</button>
       </div>
     </div>
